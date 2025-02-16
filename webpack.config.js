3b1219c45cf151c031c133a42e259d7b2ba465e2
yan-dmc <yancaio.dmc@gmail.com>
2025-02-16 20:52:19 -0300

    fix: some bug

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: './public/index.html',
+            template: './docs/index.html',
         }),
     ],
 }
