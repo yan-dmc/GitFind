83e4a8e27a3e820fc1df7105f955968b9993991d
yan-dmc <yancaio.dmc@gmail.com>
2025-02-16 21:41:50 -0300

    fix: some bug

---
+++ docs/1fb59a64cd562ba3441e.js
@@ -0,0 +1,1 @@
+import React from"react";import ReactDOM from"react-dom/client";import"./styles.css";import App from"./pages/Home";import{jsx as _jsx}from"react/jsx-runtime";var root=ReactDOM.createRoot(document.getElementById("root"));root.render(_jsx(React.StrictMode,{children:_jsx(App,{})}));
